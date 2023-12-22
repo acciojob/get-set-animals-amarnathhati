4ed4e827672342e7d9675ac68c4a0e14851cec4f
Amarnath Hati <119184772+amarnathhati@users.noreply.github.com>
2023-12-22 07:08:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 //complete this code
-class Animal {}
+class Animal {
+    makeSound() {
+        console.log("Some generic animal sound");
+    }
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+    makeSound() {
+        console.log("Woof");
+    }
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+    makeSound() {
+        console.log("Meow");
+    }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
